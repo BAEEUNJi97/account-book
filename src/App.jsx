c851f9e90fb5aac1d4187f2c00a56a3c3a63438b
Baeeunji <qormafuf97@naver.com>
2026-01-16 14:36:05 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import { Routes,Route,Link,useNavigate} from "react-router-dom";
+import Home from "./pages/Home";
+import NewTransaction from "./pages/NewTransaction";
+import EditTransaction from "./pages/EditTransaction";
+import Notfound from "./pages/Notfound";
+
+
+function App() {
+
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/new-transaction" element={<NewTransaction/>} />
+      <Route path="/edit-transaction/:id" element={<EditTransaction/>} />
+      <Route path="*" element={<Notfound/>} />
+    </Routes>  
+    </>
+  );
+}
+
+export default App;
